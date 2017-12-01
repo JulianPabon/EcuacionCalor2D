df2d9929db8ec67d40623b428be38a9030dfc756
danielfms <danielfelipe@sirius.utp.edu.co>
2017-11-30 20:37:32 -0500

    codigo en paralelo usando armadillo, codigo en paralelo sin usar armadillO y paralelizando la matriz A. Falta codigo sin usar armadillo y sin paralelizar

--- paralelo/ecuacionCalor2DEnParaleloMatrizA.cu
+++ paralelo/ecuacionCalor2DEnParaleloMatrizA.cu
@@ -3,7 +3,7 @@
 #include "arrayfire.h"
 
 using namespace std;
-using namespace arma;
+#define BLOCK_SIZE 32
 
 //Llenar la matriz A de acuerdo a la formula
 //LA MATRIZ A SOLO SE CONTRUYE UNA VEZ POR ESO NO ES 
@@ -11,16 +11,16 @@ using namespace arma;
 //Llenar la matriz A de acuerdo a la formula
 //Como los valores de los bordes ya se conocen, 
 //la matriz A solo se construye con los nodos interiores
-void llenarMatrizA(mat &A, int Nx, int Nz,double sx, double sz){
+__global__ void llenarMatrizA_CU(double *A, int nodes, int Nx, int Nz,double sx, double sz){
     double alfa = 1 + (2*sz) + (2*sx);
     int nodo, superior, inferior, derecha, izquierda;
     //Empieza en puntos z = x = 1, porque la cero es una condicion
     //de borde, si se inicia en cero la formula daria indices -1.
     //Lo anterior se aplica tambien para z = Nz -1 y  x = Nx -1
-
-    //EMPEZAR ITERADORES EN 1, 1 PARA PODER SSABER CUANDO SE VA A 
-    //ESTAR EN LOS NODOS DE FRONTERA
-    for(int j = 1; j <= Nz; j++){        // iteterar sobre filas
+    int j = threadIdx.x + blockIdx.x*blockDim.x;
+    if(j > 0 && j < nodes){ // iteterar sobre filas
+        //EMPEZAR ITERADORES EN 1, 1 PARA PODER SSABER CUANDO SE VA A 
+        //ESTAR EN LOS NODOS DE FRONTERA
         for(int i = 1; i <= Nx; i++){    // iterar sobre columnas
             nodo = ( (j-1)* Nx ) + (i-1);
             superior = nodo + Nx;
@@ -34,21 +34,21 @@ void llenarMatrizA(mat &A, int Nx, int Nz,double sx, double sz){
 
             //Ti+1,j, si i==Nx limita con un borde y por lo tanto el valor no se incluye en A
             if(i != Nx){
-                A(nodo, derecha) = -sz;
+                A[((j-1) * nodes ) + derecha] = -sz;
             }
             //Ti,j+1, si j==Nz limita con un borde y por lo tanto el valor no se incluye en A
             if(j != Nz){
-                A(nodo, superior) = -sx;
+                A[((j-1) * nodes ) + superior] = -sx;
             }
             //Ti,j
-            A(nodo, nodo) = alfa;
+            A[((j-1) * nodes ) + nodo] = alfa;
             //Ti-1,j, si i==1 limita con un borde y por lo tanto el valor no se incluye en A
             if(i != 1){
-                A(nodo, izquierda) = -sz;
+                A[((j-1) * nodes ) + izquierda] = -sz;
             }
             //Ti,j-1, si j==1 limita con un borde y por lo tanto el valor no se incluye en A
             if(j != 1){
-                A(nodo, inferior) = -sx;
+                A[((j-1) * nodes ) + inferior] = -sx;
             }
         }
     }
@@ -88,19 +88,44 @@ int main(){
 
     // ARMADILLO CREA LAS MATRICES Y VECTORES POR DEFECTO CON CEROS
     //Matriz tridiagonal A
-    mat A = mat(nodos, nodos);
+    //mat A = mat(nodos, nodos);
+    double *A = (double*)malloc(nodos * nodos * sizeof(double));
     //Vector B, puntos de la malla con temperaturas conocidas
-    vec B = vec(nodos); 
+    //vec B = vec(nodos); 
+    double *B = (double*)malloc(nodos * sizeof(double));
     //AX=B;
-    vec X = vec(nodos);
+    //vec X = vec(nodos);
+    double *X = (double*)malloc(nodos * sizeof(double));
     // Condición inicial, lugar(x,z) de la malla donde se pone la temperatura tem0
     // x -> derecha - izquierda : horizontal
     // z -> arriba - abajo : vertial
-    X((Nx * z + x)) = temp0;
-
+    X[(Nx * z + x)] = temp0;
     double sx = (k*deltaT)/(deltaX*deltaX);
     double sy = (k*deltaT)/(deltaZ*deltaZ);
-    llenarMatrizA(A, Nx, Nz, sx, sy);
+
+    double *d_A;
+    error = cudaMalloc((void**)&d_A, nodos* nodos * sizeof(double));
+    if (error != cudaSuccess) {
+        printf("Error allocating memory to d_A");
+        return 1;
+    }
+
+    cudaMemcpy(d_A, A, nodes * nodes * sizeof(float), cudaMemcpyHostToDevice);
+    int blockSize = 32;
+	dim3 dimblock(blockSize, blockSize, 1);
+    dim3 dimGrid(ceil((nodos) / float(blockSize)), ceil((nodos) / float(blockSize)), 1);
+    time_t start, end;
+    double timeGPU;
+    start = clock();
+	llenarMatrizA_CU<<<dimGrid,dimblock>>>(d_A, nodos, Nx, Nz, sx, sz);
+	cudaDeviceSynchronize();
+    end = clock();
+
+    timeGPU = difftime(end, start);
+    printf ("Elasped time in GPU: %.2lf seconds.\n", timeGPU);
+    
+    cudaMemcpy(A, d_A, nodos * nodos * sizeof(double), cudaMemcpyDeviceToHost);
+    //llenarMatrizA(A, Nx, Nz, sx, sy);
 
 
     /* CODIGO ARRAYFIRE */
@@ -110,17 +135,17 @@ int main(){
     // af::info();
 
     //Variables para pasar matriz de armadillo a arrayfire
-    double *A_mem = (double*)malloc(nodos*nodos*sizeof(double));
-    double *B_mem = (double*)malloc(nodos*sizeof(double));
-    double *X_mem = (double*)malloc(nodos*sizeof(double));
+    // double *A_mem = (double*)malloc(nodos*nodos*sizeof(double));
+    // double *B_mem = (double*)malloc(nodos*sizeof(double));
+    // double *X_mem = (double*)malloc(nodos*sizeof(double));
     //Pasar matrices a punteros de c++
-    A_mem = A.memptr();
-    B_mem = B.memptr();
-    X_mem = X.memptr();
+    // A_mem = A.memptr();
+    // B_mem = B.memptr();
+    // X_mem = X.memptr();
     //Creacion de arrays en ArrayFire
-    af::array afA(nodos,nodos,A_mem);
-    af::array afB(nodos,f64);
-    af::array afX(nodos,f64);
+    af::array afA(nodos,nodos,A);
+    af::array afB(nodos,B);
+    af::array afX(nodos,B;
 
     //Poner temperatura inicial
 
