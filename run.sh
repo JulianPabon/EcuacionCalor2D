53d7ae20907cba90c24c8a2b1825e272bdc26429
JulianHPC <julianpabonb10@gmail.com>
2017-12-06 00:23:12 -0500

    tiempos con 10 iteraciones para cada entrada

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #No esta funcionando {0..9}
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-	for j in 1 2 3 4 5
+	for j in 1 2 3 4 5 6 7 8 9 10
 	do
 		./secuencial/EC_2D < ./in/in$i >> ./times/secuencial/times$i.txt
         #./paralelo/build/EC_Par < ./in/in$i >> ./times/paralelo/times$i.txt
