9ca7fc2f7a8189d93a040ecbb208019778ca4d1e
JulianHPC <julianpabonb10@gmail.com>
2017-12-05 22:39:27 -0500

    script para ejecutar y tomar  los tiempos del codigo secuencial para todas las entradas

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-for i in {0..4}
+#No esta funcionando {0..9}
+for i in 0 1 2 3 4 5 6 7 8 9
 do
-	for j in {1..5}
+	for j in 1 2 3 4 5
 	do
-		./EC_Sec < ./in/in$i >> ./times/secuencial/times$i.txt
+		./secuencial/EC_2D < ./in/in$i >> ./times/secuencial/times$i.txt
         #./paralelo/build/EC_Par < ./in/in$i >> ./times/paralelo/times$i.txt
 	done
 	echo "Tiempos finalizados con "in$i
